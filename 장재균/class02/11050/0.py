1a2ce65b04f611ec2a7755a07c9e0023552ffbfa
JangJaeKyun <129075871+JangJaeKyun@users.noreply.github.com>
2023-05-22 11:03:14 +0900

    11050 이항 계수1

---
+++ 장재균/class02/11050/0.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def bino_coef(n, k):
+    if k == 0 or n == k:
+        return 1
+    return bino_coef(n-1, k) + bino_coef(n-1, k-1)
+
+n, k = map(int, input().split())
+
+print(bino_coef(n, k))
