c0bfe3f533ba9951ab6a05ad382129ff5b7d51c9
Pyotato <pyolovely01@gmail.com>
2023-05-28 09:19:41 +0900

    2023.05.28 | 1620 | pyotato : solved

---
+++ 표혜민/class03/WEEK03/1620/0.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/1620
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())  # 1<=N,M<=100000
+p1, p2 = dict(), dict()
+for i in range(1, N+1):
+    pkm = input().rstrip()
+    p1[str(i)] = pkm
+    p2[pkm] = str(i)
+
+for _ in range(M):
+    q = input().rstrip()
+    print(p1[q]) if q.isdigit() else print(p2[q])
