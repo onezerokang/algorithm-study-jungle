ee07bddf13a64959a1470cbf3ae5411b0173ba87
Pyotato <pyolovely01@gmail.com>
2023-05-28 09:32:31 +0900

    update solved state in readme

--- 표혜민/class03/WEEK03/1620/0.py
+++ 표혜민/class03/WEEK03/1620/0.py
@@ -1,4 +1,11 @@
 # https://www.acmicpc.net/problem/1620
+#logic
+# p1은 번호를 키로 하여 바로 dict에 접근하도록,
+# p2는 포켓몬 이름을 키로 하여 바로 접근하도록 풀이
+# 단, 번호는 1번부터 시작하며 
+# 시험문제가 번호로 주어졌을때는 p1에 접근, 이름으로 주어졌을때는 p2에 접근하도록 하기
+
+
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())  # 1<=N,M<=100000
