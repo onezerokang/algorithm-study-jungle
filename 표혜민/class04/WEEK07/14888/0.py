b53a8b903096ab894297eb9a1d1b8dfd46281569
Pyotato <pyolovely01@gmail.com>
2023-06-20 21:59:23 +0900

    14888 풀이 완료

---
+++ 표혜민/class04/WEEK07/14888/0.py
@@ -0,0 +1,36 @@
+from sys import stdin
+from collections import deque
+from itertools import permutations
+N = int(stdin.readline().strip())
+nums = list(map(int, stdin.readline().strip().split()))
+nums = deque(nums)
+calc = dict()
+calc["+"], calc["-"], calc["*"], calc["/"] = list(
+    map(int, stdin.readline().strip().split()))
+ops = {"+": (lambda a, b: a+b), "-": (lambda a, b: a-b),
+       "*": (lambda a, b: a*b), "/": (lambda a, b: a//b if a*b>0 else -(abs(a)//abs(b)))}
+if N == 2: #연산할 수가 2개 밖에 없을 경우
+    for i in calc:
+        if calc[i] > 0:
+            print(ops[i](*nums))
+            print(ops[i](*nums))
+else:
+    ans = []
+    dq = deque(set(permutations([*["+"]*calc["+"],*["*"]*calc["*"],*["-"]*calc["-"],*["/"]*calc["/"]],N-1)))
+    while dq:
+        calcs = dq.popleft()
+        idx = 0
+        temp =deque()
+        for i in nums:
+            if not temp:
+                temp.append(i)
+                continue
+            if len(temp)==1:
+                temp.append(i)
+            if len(temp)==2: 
+                a,b = temp.popleft(),temp.popleft()
+                temp.append(ops[calcs[idx]](*[a,b]))
+                idx+=1
+        ans.append(*temp)
+    print(max(ans))
+    print(min(ans))
